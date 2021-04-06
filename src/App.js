f89f07fbd1acec73050abce302d25c181d427bb1
Aashiish-Pandey <ash006pandey@gmail.com>
2021-04-06 15:10:22 +0530

    upadating state without using setState()

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React , {Component} from 'react'
+import ReactDOM from  'react-dom'
+import Counter from './Component/Counter'
+import './App.css'
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <div className = "App">
+
+        <Counter/>
+      </div>
+    )
+  }
+
+} 
+
+export default App;
